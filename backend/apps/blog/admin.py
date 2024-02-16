d733244d34a2ff4412297119dcebe3d23ff1c5a3
bekowashere <uberke.karatas@gmail.com>
2024-02-16 21:02:52 +0300

    blog app models & admin

--- backend/apps/blog/admin.py
+++ backend/apps/blog/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.utils.html import format_html
+from parler.admin import TranslatableAdmin
+from apps.blog.models import Category, Post
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(TranslatableAdmin):
+    fieldsets = (
+        (_('Basic Information'), {'fields': ('title', 'summary')}),
+        (_('Slug'), {'fields': ('slug',)}),
+        (_('Post List'), {'fields': ('show_posts',)}),
+    )
+    list_display = ('title', 'slug', 'show_post_count')
+    search_fields = ('title__icontains', 'slug')
+    # ordering = ('translations__title',)
+    # readonly_fields = ('post_count',)
+    readonly_fields = ('show_posts',)
+
+    def show_post_count(self, obj):
+        result = Post.objects.filter(category=obj).count()
+        format = format_html("<b>{}</b>", result)
+        return format
+
+    show_post_count.short_description = _("Post Count")
+
+    def show_posts(self, obj):
+        posts = Post.objects.filter(category=obj)
+        post_links = [format_html('<a target="_blank" href="/admin/blog/post/{}/change/">{}</a>', post.id, post.title) for post in posts]
+        return format_html('<br>'.join(post_links))
+    
+    show_posts.short_description = _("List")
+
+    
+
+@admin.register(Post)
+class PostAdmin(TranslatableAdmin):
+    fieldsets = (
+        (_('Main Information'), {'fields': ('author', 'category', 'title')}),
+        (_('Status'), {'fields': ('status',)}),
+        (_('Content'), {'fields': ('image', 'content')}),
+        (_('Metadata'), {'fields': ('created_at', 'updated_at')}),
+    )
+    list_display = ('title', 'slug', 'category', 'status')
+    list_filter = ('status',)
+    search_fields = ('title__icontains', 'slug')
+    readonly_fields = ('created_at', 'updated_at')
--- backend/apps/blog/models.py
+++ backend/apps/blog/models.py
@@ -1,2 +1,96 @@
 from django.db import models
-# Create your models here.
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import FileExtensionValidator
+from parler.models import TranslatableModel, TranslatedFields
+
+class Category(TranslatableModel):
+    translations = TranslatedFields(
+        title = models.CharField(_('Title'), max_length=128),
+        summary = models.TextField(_('Summary'), null=True, blank=True)
+    )
+    slug = models.SlugField(_('Slug'), unique=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    @property
+    def post_count(self):
+        return self.category_posts.all().count()
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs) 
+    
+    class Meta:
+        ordering = ('translations__title',)
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+def upload_post_image(instance, filename):
+    filebase, extension = filename.split(".")
+    return "{}/{}.{}".format("blog", instance.slug, extension)
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Published"),
+)
+
+class Post(TranslatableModel):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="user_posts",
+        verbose_name=_('Author')
+    )
+
+    status = models.PositiveSmallIntegerField(_("Status"), choices=STATUS, default=0)
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name="category_posts",
+        null=True,
+        verbose_name=_('Category')
+    )
+
+    # Multi-Langugage
+    translations = TranslatedFields(
+        title = models.CharField(_('Title'), max_length=255),
+        content = models.TextField(_('Content'), null=True, blank=True)
+    )
+
+    slug = models.SlugField(_('Slug'), unique=True, blank=True)
+
+    # image
+    image = models.ImageField(
+        _("Image"),
+        upload_to=upload_post_image,
+        validators=[FileExtensionValidator(["png", "jpg", "jpeg"])],
+        null=True,
+        blank=True,
+    )
+
+    # Metadata
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    # def get_absolute_url(self):
+    #     return reverse("academy:university-detail", kwargs={"university_id": self.university_id})
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)  
+    
+    class Meta:
+        ordering = ("-created_at",)
+        verbose_name = _('Post')
+        verbose_name_plural = _('Posts')
